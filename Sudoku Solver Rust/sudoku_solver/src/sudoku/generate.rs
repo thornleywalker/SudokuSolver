a99507f18594170cbb14bd6be636876b8ad09095
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-08-10 14:53:24 -0600

    removed http stuff

--- Sudoku Solver Rust/sudoku_solver/src/sudoku/generate.rs
+++ Sudoku Solver Rust/sudoku_solver/src/sudoku/generate.rs
@@ -1,6 +1,4 @@
 use {
-    reqwest,
-    scraper::{Selector, Html},
     crate::sudoku, 
     std::collections::HashMap
 };
@@ -11,37 +9,11 @@ pub enum Difficulty{
     Hard,
     Expert
 }
-pub fn generate_board(difficulty: Difficulty) -> Option<sudoku::Board>
+pub fn generate_board(difficulty: Difficulty) -> sudoku::Board
 {
-    let resp = reqwest::blocking::get(
-            match difficulty{
-            Easy => "https://sudoku.com/easy/",
-            Medium => "https://sudoku.com/medium/",
-            Hard => "https://sudoku.com/hard/",
-            Expert => "https://sudoku.com/expert/",
-            }
-        ).ok()?;
-
-    
-
-    let body = resp.text().unwrap();
-    
-    let document = Html::parse_document(&body);
-    let selector = Selector::parse("#game").unwrap();
-
-    let something = document.select(&selector);
-
-    for element in something{
-        println!("got something!");
-        println!("{:?}", element);
-    }
-
-
-
-
     let mut in_string = String::new();
 
     let mut return_board = sudoku::Board::from_list(in_string);
 
-    Some(return_board)
+    return_board
 }
