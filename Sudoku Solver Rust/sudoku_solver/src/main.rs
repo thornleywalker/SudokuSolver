373963ccd987e3af3f66b35d5037722e5d948d65
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-08-12 12:01:13 -0600

    added test board

--- Sudoku Solver Rust/sudoku_solver/src/main.rs
+++ Sudoku Solver Rust/sudoku_solver/src/main.rs
@@ -1,8 +1,13 @@
 use sudoku;
 
 fn main() {
-    sudoku::Board::new();
+    let mut new_board = 
+        sudoku::Board::from_list(
+            String::from("000520,000400001,000080900,060000004,070302085,038070,900,050003,007100042"));
 
-
-    println!("Hello, world!");
+    if new_board.solve(){
+        println!("the new board was solved");
+        new_board.to_display();
+    }
+    else { println!("failure"); }
 }
